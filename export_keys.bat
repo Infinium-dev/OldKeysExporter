2fa8258a884ae4e8c219faa219ae06a5dc75567b
Infinium-dev <infiniumcoindev@seznam.cz>
2020-11-26 10:22:06 +0100

    Fixed bugs caused by previous commit

--- export_keys.bat
+++ export_keys.bat
@@ -1,18 +1,23 @@
-@echo off
+@echo off &setlocal enabledelayedexpansion
 cls
 set /p original_path=Enter path of your old wallet (.keys file): 
-set /p wallet_pass_original=Enter wallet password: 
+set /p "wallet_pass_original=Enter wallet password:"
 if /I "%wallet_pass_original%"=="" goto wallet_no_pass
 set "wallet_pass=!wallet_pass_original:&=^&!"
+setlocal DisableDelayedExpansion
+
+echo Your password is: %wallet_pass%
+
+set bin_pass=0
 copy %original_path% old_wallet.keys
 echo @echo off >run_forknote.bat
-echo "windows_bin/forknote_simplewallet.exe" --config-file=config/inf.conf --wallet-file=old_wallet.keys --password=%wallet_pass% >>run_forknote.bat
+echo "windows_bin/forknote_simplewallet.exe" --config-file=config/inf.conf --wallet-file=old_wallet.keys --password="%wallet_pass_original%" >>run_forknote.bat
 echo exit >>run_forknote.bat
 start run_forknote.bat
 timeout 15 /nobreak >nul
 taskkill /f /im forknote_simplewallet.exe
 timeout 1 /nobreak >nul
-(echo %wallet_pass% & echo ) | "windows_bin/bytecoin_walletd.exe" --wallet-file=old_wallet.wallet --export-keys
+"windows_bin/bytecoin_walletd.exe" --wallet-file=old_wallet.wallet --export-keys --wallet-password="%wallet_pass_original%"
 echo keys exported press any key to exit
 pause >nul
 del old_wallet.wallet
